2ef88c230bec2194bdf8562007182a01037bd770
Zain Amro <zainamro1@gmail.com>
2014-03-05 19:15:10 -0800

    added some front-end work

--- sectionswitcher/switcher/views.py
+++ sectionswitcher/switcher/views.py
@@ -6,21 +6,10 @@ from switcher.models import *
 from django.core.urlresolvers import reverse
 
 def home(request):
-	if request.method == 'POST':
-		form = SectionSwitchForm(request.POST)
-		if form.is_valid():
-			all_sections = Section.objects.all()
-			current_section_id = request.POST['current_section']
-			desired_section_id = request.POST['desired_section']
-			student = Student(request.POST['email'], all_sections[current_section_id], all_sections[desired_section_id])
-			student.save()
-			return HttpResponseRedirect(reverse('thanks'))
-	else:
-		form = SectionSwitchForm()
-	
-	return render(request, "switcher/home.html", {"form": form})
+	return render_to_response("switcher/home.html")
 
 def thanks(request):
-	return HttpResponse("Thanks!")
+	if request.method == 'GET':
+		return HttpResponse(request.GET['departments'])
 
 		
